42725dad82c43aba9e9df8e291a8329cfb718b65
Anish Shrestha <anish@opalinetech.com>
2019-06-06 11:55:56 +0545

    added header footer and index

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+<!-- JQuery -->
+<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="assets/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="assets/js/mdb.min.js"></script>
+<!-- Initializations -->
+<script type="text/javascript" src="assets/js/custom.js"></script>
+<!-- data-table -->
+<script type="text/javascript" src="assets/js/addons/datatables.min.js"></script>
+<!-- owl Carousel -->
+<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="assets/css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="assets/css/style.min.css" rel="stylesheet">
+  <!-- datatables -->
+  <link rel="stylesheet" type="text/css" href="assets/css/addons/datatables.min.css">
+  <!-- hover.css -->
+  <link href="assets/css/hover.css" rel="stylesheet">
+  <!-- owl Carousel -->
+  <link href="assets/css/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/css/owl.theme.default.min.css" rel="stylesheet">
+
+  <link href="assets/css/custom.css" rel="stylesheet">
+</head>
+<body>
+  <!-- navigation bar start-->
+  <nav class="mb-1 navbar navbar-expand-lg navbar-dark secondary-color lighten-1">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-5" aria-controls="navbarSupportedContent-5" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-5">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link waves-effect waves-light" href="#">Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link waves-effect waves-light" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link waves-effect waves-light" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown
+          </a>
+          <div class="dropdown-menu dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-5">
+            <a class="dropdown-item waves-effect waves-light" href="#">Action</a>
+            <a class="dropdown-item waves-effect waves-light" href="#">Another action</a>
+            <a class="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto nav-flex-icons">
+        <li class="nav-item">
+          <a class="nav-link waves-effect waves-light">1
+            <i class="fas fa-envelope"></i>
+          </a>
+        </li>
+        <li class="nav-item avatar dropdown">
+          <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg" class="rounded-circle z-depth-0" alt="avatar image">
+          </a>
+          <div class="dropdown-menu dropdown-menu-lg-right dropdown-secondary" aria-labelledby="navbarDropdownMenuLink-5">
+            <a class="dropdown-item waves-effect waves-light" href="#">Action</a>
+            <a class="dropdown-item waves-effect waves-light" href="#">Another action</a>
+            <a class="dropdown-item waves-effect waves-light" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- navigation bar end -->
